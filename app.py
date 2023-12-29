56b998793defda8bc1548b6f4096e4a58afba776
thippeswamy123-ship-it <thippeswamy@shikshalokam.org>
2023-12-29 14:37:50 +0530

    config file added

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file  # Add the 'send_file' import
+from flask import Flask, render_template, request, redirect, url_for, send_file
 import os
 import sqlite3
 import pandas as pd
+from configparser import ConfigParser
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
+
+config = ConfigParser()
+config.read('config.ini')
+QUERIES = dict(config['QUERIES'])
+
+def run_query(conn, query):
+    result = pd.read_sql_query(query, conn)
+    return result.iloc[0, 0]
+
+def convert_excel_to_csv(excel_path):
+    csv_path = os.path.splitext(excel_path)[0] + '.csv'
+    excel_data = pd.read_excel(excel_path)
+    excel_data.to_csv(csv_path, index=False)
+    return csv_path
 
 @app.route('/')
 def index():
@@ -20,14 +35,16 @@ def upload():
         return redirect(request.url)
 
     if file:
-        # Ensure the "uploads" directory exists
         if not os.path.exists(app.config['UPLOAD_FOLDER']):
             os.makedirs(app.config['UPLOAD_FOLDER'])
 
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(file_path)
 
-        # Import CSV to SQLite
+        if file.filename.lower().endswith('.xlsx'):
+            # Convert XLSX to CSV
+            file_path = convert_excel_to_csv(file_path)
+
         conn = sqlite3.connect('data.db')
         df = pd.read_csv(file_path)
         df.to_sql('data_table', conn, index=False, if_exists='replace')
@@ -37,58 +54,43 @@ def upload():
 
 @app.route('/analyze')
 def analyze():
-    # Connect to SQLite and run queries
     conn = sqlite3.connect('data.db')
 
-    # Query 1: Count the total number of rows
-    total_rows_query = "SELECT COUNT(*) FROM data_table"
-    total_rows_result = pd.read_sql_query(total_rows_query, conn)
-    total_rows = total_rows_result.iloc[0, 0]
+    total_rows = run_query(conn, QUERIES['total_rows'])
+    unique_uuids_count = run_query(conn, QUERIES['unique_uuids'])
+    task_level_evidence = run_query(conn, QUERIES['find_evidence'])
+    task_level_evidence1 = run_query(conn, QUERIES['find_evidencemorethan'])
 
-    # Query 2: Count the number of unique UUIDs
-    unique_uuids_query = "SELECT COUNT(DISTINCT UUID) FROM data_table"
-    unique_uuids_result = pd.read_sql_query(unique_uuids_query, conn)
-    unique_uuids_count = unique_uuids_result.iloc[0, 0]
-
-    # Query 3: Count the number of unique UUIDs
-    find_evidence = "SELECT COUNT(DISTINCT UUID) AS num_users_with_task_evidences FROM data_table WHERE Task_Evidence like '%http%';"
-    uuids_result = pd.read_sql_query(find_evidence, conn)
-    task_level_evidence = uuids_result.iloc[0, 0]
-
-    # Close the database connection
     conn.close()
 
-    return render_template('result.html', total_rows=total_rows, unique_uuids_count=unique_uuids_count, task_level_evidence=task_level_evidence)
+    return render_template('result.html', total_rows=total_rows, unique_uuids_count=unique_uuids_count, task_level_evidence=task_level_evidence, task_level_evidence1=task_level_evidence1)
 
 @app.route('/download_metrics')
 def download_metrics():
-    # Connect to SQLite and run queries
     conn = sqlite3.connect('data.db')
 
-    # Query your metrics
-    total_rows_query = "SELECT COUNT(*) FROM data_table"
-    unique_uuids_query = "SELECT COUNT(DISTINCT UUID) FROM data_table"
-    find_evidence_query = "SELECT COUNT(DISTINCT UUID) AS num_users_with_task_evidences FROM data_table WHERE Task_Evidence LIKE '%http%';"
+    total_rows = run_query(conn, QUERIES['total_rows'])
+    unique_uuids_count = run_query(conn, QUERIES['unique_uuids'])
+    task_level_evidence = run_query(conn, QUERIES['find_evidence'])
+    task_level_evidence1 = run_query(conn, QUERIES['find_evidencemorethan'])
 
-    total_rows_result = pd.read_sql_query(total_rows_query, conn)
-    unique_uuids_result = pd.read_sql_query(unique_uuids_query, conn)
-    uuids_result = pd.read_sql_query(find_evidence_query, conn)
+    total_rows_result = run_query(conn, total_rows)
+    unique_uuids_result = run_query(conn, unique_uuids_count)
+    uuids_result = run_query(conn, task_level_evidence)
+    uuids_1task = run_query(conn, task_level_evidence1)
 
-    # Combine results into a DataFrame
     metrics_df = pd.DataFrame({
-        'Total Submissions': [total_rows_result.iloc[0, 0]],
-        'Unique UUIDs': [unique_uuids_result.iloc[0, 0]],
-        'Task Level Evidences': [uuids_result.iloc[0, 0]],
+        'Total Submissions': [total_rows_result],
+        'Unique UUIDs': [unique_uuids_result],
+        'Task Level Evidences': [uuids_result],
+        'Task Level Evidences 1': [uuids_1task]
     })
 
-    # Save the DataFrame to a CSV file
     metrics_csv_path = os.path.join(app.config['UPLOAD_FOLDER'], 'metrics.csv')
     metrics_df.to_csv(metrics_csv_path, index=False)
 
-    # Close the database connection
     conn.close()
 
-    # Send the file as a response
     return send_file(metrics_csv_path, as_attachment=True)
 
 if __name__ == '__main__':
